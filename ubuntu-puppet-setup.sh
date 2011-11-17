7c1b9fe0504af647476b406d902294f2e89eccda
Jeroen Bourgois <jeroenbourgois@gmail.com>
2011-11-17 11:56:15 +0100

    update

--- ubuntu-puppet-setup.sh
+++ ubuntu-puppet-setup.sh
@@ -11,7 +11,7 @@ COL_RESET="\x1b[39;49;00m"
 COL_RED="\x1b[31;01m"
 
 _log() {
-  _print "$1 ******************************************"
+  _print "$1 *****************"
 }
 
 _print() {
@@ -19,7 +19,7 @@ _print() {
 }
 
 _error() {
-  _print $COL_RED"Error:\n$1\n" >> /root/manifest
+  _printf $COL_RED"Error:\n$1\n" >> /root/manifest
 }
 
 # check for root
@@ -33,22 +33,14 @@ _log "Preparing machine for puppet, started on `date`"
 _log ""
 
 _log "Install dependencies"
-apt-get install libopenssl-ruby rdoc libopenssl-ruby1.8 libreadline-ruby1.8 libruby1.8 rdoc1.8 ruby1.8 git
+apt-get --force-yes install libopenssl-ruby rdoc libopenssl-ruby1.8 libreadline-ruby1.8 libruby1.8 rdoc1.8 ruby1.8 git
 
 _log "Get puppet and facter"
-_log "Files coming from http://puppetlabs.com/misc/download-options/"
-_log "Last version here is from 17/11/2011"
-cd /tmp
-wget http://puppetlabs.com/downloads/facter/facter-1.6.3.tar.gz
-wget http://puppetlabs.com/downloads/puppet/puppet-2.7.6.tar.gz
-tar xvzf facter-1.6.3.tar.gz
-cd facter-1.6.3
-ruby install.rb
-cd ..
-tar xvzf puppet-2.7.6.tar.gz
-cd puppet-2.7.6
-ruby install.rb
-puppetd --version > /root/manifest
+add-apt-repository ppa:mathiaz/puppet-backports
+apt-get update
+apt-get --force-yes install puppet puppetmaster
+
+puppetd --version >> /root/manifest
 
 _log "Import puppet cookbook"
 _log "Delete puppet dir if it exists, possible due to install"
